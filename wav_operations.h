cab8836090d552d5be10103d284e9ca544f32ff9
Sachko151 <sachko151@abv.bg>
2023-04-15 15:55:08 +0300

    Added the functionality, should refractor before merging

--- wav_operations.h
+++ wav_operations.h
@@ -3,6 +3,7 @@
 #include "wav_file_structure.h"
 #include <stdio.h>
 #include <stdlib.h>
+FILE *open_wav_file_and_return_error_if_encountered(char *filename);
 void print_out_wav_file_metadata_easy_way(wav_header_t wav_struct);
 void print_out_wav_file_metadata_harder_way(wav_header_t *wav_struct, FILE *file_pointer);
 #endif
